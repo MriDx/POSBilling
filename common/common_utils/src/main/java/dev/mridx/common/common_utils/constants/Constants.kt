ca406e18635e04e67f97a8be9f010d3b41d07eb3
baishyamridul <mridul.sumatoglobal@gmail.com>
2023-06-30 16:56:19 +0530

    notification settings added

--- common/common_utils/src/main/java/dev/mridx/common/common_utils/constants/Constants.kt
+++ common/common_utils/src/main/java/dev/mridx/common/common_utils/constants/Constants.kt
@@ -8,5 +8,6 @@ object Permissions {
     const val LOCATION = "location_permission"
     const val NOTIFICATION_PERMISSION = "notification_permission"
     const val NOTIFICATION_PERMISSION_PROMPT = "notification_permission_prompt"
+    const val NOTIFICATION_ENABLED = "notification_enabled"
 
 }
