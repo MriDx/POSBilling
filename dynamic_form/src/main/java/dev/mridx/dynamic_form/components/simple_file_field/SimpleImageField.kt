b247cbaff38984eeb9e9a1bc1120d9fb8e7fda64
baishyamridul <mridul.sumatoglobal@gmail.com>
2023-08-17 12:30:49 +0530

    dynamic form improvement

--- dynamic_form/src/main/java/dev/mridx/dynamic_form/components/simple_file_field/SimpleImageField.kt
+++ dynamic_form/src/main/java/dev/mridx/dynamic_form/components/simple_file_field/SimpleImageField.kt
@@ -1,52 +1,23 @@
 package dev.mridx.dynamic_form.components.simple_file_field
 
 import android.content.Context
-import android.content.DialogInterface
 import android.net.Uri
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.LinearLayoutCompat
-import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
-import dev.mridx.common.common_utils.file_utils.GetUriOfFiles
 import dev.mridx.dynamic_form.R
 import dev.mridx.dynamic_form.databinding.ImageUploadFieldBinding
-import dev.mridx.image_crop.ImageCropActivity
-import java.io.File
 
 class SimpleImageField : LinearLayoutCompat, SimpleFileField {
 
 
-    private fun getActivityReference(): AppCompatActivity {
-        return (context as AppCompatActivity)
-    }
+    var onCameraAction: (() -> Unit)? = null
 
-
-    private val imagePickerLauncher =
-        getActivityReference().registerForActivityResult(ActivityResultContracts.GetContent()) {
-            it?.let {
-                imageCropActivityLauncher.launch(it)
-            }
-        }
-
-    private var imageCaptureFileUri: Uri? = null
-    private val imageCaptureLauncher =
-        getActivityReference().registerForActivityResult(ActivityResultContracts.TakePicture()) { captured ->
-            if (captured && imageCaptureFileUri != null) {
-                imageCropActivityLauncher.launch(imageCaptureFileUri)
-            }
-        }
-
-
-    private val imageCropActivityLauncher =
-        (context as AppCompatActivity).registerForActivityResult(ImageCropActivity.ImageCropActivityContract()) {
-            onCroppedResult(it)
-        }
+    var onPickerAction: (() -> Unit)? = null
 
 
     private val binding =
@@ -82,6 +53,14 @@ class SimpleImageField : LinearLayoutCompat, SimpleFileField {
         binding.headingView.text = heading
     }
 
+    override fun setHint(hint: String) {
+        binding.uploadHint.text = hint
+    }
+
+    override fun setValue(value: String) {
+        //
+    }
+
 
     private var imageUri: Uri? = null
     fun setImage(uri: Uri) {
@@ -114,79 +93,68 @@ class SimpleImageField : LinearLayoutCompat, SimpleFileField {
         return true
     }
 
+    override fun showErrorMessage(errorMessage: String) {
+        //
+    }
+
     override fun getValue(): String {
         return imageUri?.path ?: ""
-    }
-
-
-    private fun showPickerDialog() {
-        AlertDialog.Builder(context).apply {
-            setTitle("Select source")
-            setItems(arrayOf("Open Camera", "Select from Gallery"),
-                DialogInterface.OnClickListener { dialog, btnIndex ->
-                    dialog.dismiss()
-                    if (btnIndex == 0) {
-                        //
-                        openCamera()
-                    } else {
-                        //
-                        imagePickerLauncher.launch("image/*")
-                    }
-                }
-            )
-            setCancelable(true)
-            setPositiveButton("Cancel") { dialog, index ->
-                dialog.dismiss()
-            }
-        }.show()
-    }
-
-    private fun openCamera() {
-
-        imageCaptureFileUri = getTmpFileUri()
-
-        imageCaptureLauncher.launch(imageCaptureFileUri)
-
-
-    }
-
-
-    /**
-     * use this function to generate uri for capture image
-     */
-    private fun getTmpFileUri(): Uri {
-        val tmpFile =
-            File.createTempFile("tmp_image_file", ".png", getActivityReference().cacheDir).apply {
-                createNewFile()
-                deleteOnExit()
-            }
-
-        return GetUriOfFiles.getUri(context, tmpFile)
-    }
-
-
-    private fun onCroppedResult(fileUri: Uri?) {
-        if (fileUri != null) {
-            val file = File(fileUri.path)
-            setImage(uri = file.toUri())
-        }
     }
 
     override fun getName(): String {
         return ""
     }
 
-    override fun showErrorMessage(errorMessage: String) {
-        //
+    fun setError(errorMessage: String) {
+        if (errorMessage.isEmpty()) return
+        binding.errorView.apply {
+            text = errorMessage
+        }.isVisible = true
+    }
+
+    enum class OPTIONS {
+        CAMERA_ONLY, GALLERY_ONLY, BOTH
+    }
+
+    private var sourceOptions = arrayOf("Open Camera", "Select from Gallery")
+
+    fun setOptions(option: OPTIONS) {
+        when (option) {
+            OPTIONS.CAMERA_ONLY -> {
+                sourceOptions = arrayOf("Open Camera")
+            }
+
+            OPTIONS.GALLERY_ONLY -> {
+
+            }
+
+            else -> {
+
+            }
+        }
     }
 
 
-    override fun setHint(hint: String) {
-        //
-    }
-
-    override fun setValue(value: String) {
-        //
+    private fun showPickerDialog() {
+        AlertDialog.Builder(context).apply {
+            setTitle("Select source")
+            setItems(
+                sourceOptions
+            ) { dialog, btnIndex ->
+                dialog.dismiss()
+                if (btnIndex == 0) {
+                    //
+                    onCameraAction?.invoke()
+                } else {
+                    //
+                    onPickerAction?.invoke()
+                }
+            }
+            setCancelable(true)
+            setPositiveButton("Cancel") { dialog, index ->
+                dialog.dismiss()
+            }
+        }.show()
     }
 
 }
